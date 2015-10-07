6f4b0b44f21378add3cd972be6859be68a9d689d
JafeChang <bestzjf001@163.com>
2015-10-07 22:50:59 +0800

    add post successfully

--- src/info/jafe/emboard/controller/PostController.java
+++ src/info/jafe/emboard/controller/PostController.java
@@ -1,15 +1,26 @@
 package info.jafe.emboard.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import info.jafe.emboard.entity.User;
+import info.jafe.emboard.service.PostService;
+
 @Controller
 public class PostController {
+
+	@Autowired
+	private PostService postService;
+
 	@RequestMapping("addpost")
-	public String addpost(@RequestParam String topic, @RequestParam String body) {
-		System.out.println(topic);
-		System.out.println(body);
+	public String addpost(@RequestParam String topic, @RequestParam String body, HttpSession session) {
+		User user = (User) session.getAttribute("user");
+		int id = user.getId();
+		postService.add(0, topic, body, "", id);
 		return "index";
 	}
 
